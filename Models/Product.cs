66173e86129ab123eec541fe64e7a4b091be5aae
Bastian Asmussen <bastianasmussen@gmail.com>
2023-08-21 11:24:06 +0200

    Added.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,30 +1,84 @@
-﻿namespace Products.Models;
+﻿using Avalonia;
 
-public class Product
+namespace Products.Models;
+
+public class Product : AvaloniaObject
 {
     public Product(string name, string description, decimal salesPrice, decimal purchasePrice, float stock,
         StockUnit stockUnit, bool isHidden = false)
     {
         Name = name;
         Description = description;
-        
+
         SalesPrice = salesPrice;
         PurchasePrice = purchasePrice;
-        
+
         Stock = stock;
         StockUnit = stockUnit;
-        
+
         IsHidden = isHidden;
     }
-    
-    public string Name { get; set; }
-    public string Description { get; set; }
 
-    public decimal SalesPrice { get; set; }
-    public decimal PurchasePrice { get; set; }
+    public string Name
+    {
+        get => GetValue(NameProperty);
+        set => SetValue(NameProperty, value);
+    }
 
-    public float Stock { get; set; }
-    public StockUnit StockUnit { get; set; }
-    
-    public bool IsHidden { get; set; }
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public decimal SalesPrice
+    {
+        get => GetValue(SalesPriceProperty);
+        set => SetValue(SalesPriceProperty, value);
+    }
+
+    public decimal PurchasePrice
+    {
+        get => GetValue(PurchasePriceProperty);
+        set => SetValue(PurchasePriceProperty, value);
+    }
+
+    public float Stock
+    {
+        get => GetValue(StockProperty);
+        set => SetValue(StockProperty, value);
+    }
+
+    public StockUnit StockUnit
+    {
+        get => GetValue(StockUnitProperty);
+        set => SetValue(StockUnitProperty, value);
+    }
+
+    public bool IsHidden
+    {
+        get => GetValue(IsHiddenProperty);
+        set => SetValue(IsHiddenProperty, value);
+    }
+
+    public static readonly StyledProperty<string> NameProperty =
+        AvaloniaProperty.Register<Product, string>(nameof(Name));
+
+    public static readonly StyledProperty<string> DescriptionProperty =
+        AvaloniaProperty.Register<Product, string>(nameof(Description));
+
+    public static readonly StyledProperty<decimal> SalesPriceProperty =
+        AvaloniaProperty.Register<Product, decimal>(nameof(SalesPrice));
+
+    public static readonly StyledProperty<decimal> PurchasePriceProperty =
+        AvaloniaProperty.Register<Product, decimal>(nameof(PurchasePrice));
+
+    public static readonly StyledProperty<float> StockProperty =
+        AvaloniaProperty.Register<Product, float>(nameof(Stock));
+
+    public static readonly StyledProperty<StockUnit> StockUnitProperty =
+        AvaloniaProperty.Register<Product, StockUnit>(nameof(StockUnit));
+
+    public static readonly StyledProperty<bool> IsHiddenProperty =
+        AvaloniaProperty.Register<Product, bool>(nameof(IsHidden));
 }
